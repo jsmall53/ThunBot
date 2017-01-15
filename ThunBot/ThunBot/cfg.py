c1bea8b8c2f6bdb7563126cf875c2c4553a37713
Jordan Small <jsmall53@ymail.com>
2017-01-15 14:40:04 -0800

    Updated file paths

--- ThunBot/ThunBot/cfg.py
+++ ThunBot/ThunBot/cfg.py
@@ -5,19 +5,19 @@ HOST = "irc.twitch.tv"  #twitch IRC server
 PORT = 6667
 NICK = "thunbot_" #name of the bot
 PASS = "oauth:zq951d4x3w1vdu9mzf8hc3a730ily1"#"oauth:xxi5yk7t1ul1q3wptdklgvrrdf4qls" #password to bot account
-CHAN = "#etup" #the channel you want to join
+CHAN = "#bigsmcgee" #the channel you want to join
 #RATE =  (20/30)         #chat spam limit
 
 THUNBEAST = "ThunBeast"
 BOTNAME = ["ThunBot_", "ThunBot_,"] #make sure both version of this exist since twitch autocomplete puts a comma at the end of the name
 
 #EMOTESLIST FILEPATH
-FILEPATH = "D:\\git\\ThunBot\\ThunBot\\"
+FILEPATH = "D:\\git\\ThunBot\\ThunBot\\ThunBot\\"
 EMOTE_LIST = r"EmoteList.txt"
 fileName = FILEPATH+EMOTE_LIST
 
 #GUESS WINS FILEPATH
-GUESSFILE = "D:\git\ThunBot\ThunBot\GuessWins.txt"
+GUESSFILE = "D:\git\ThunBot\ThunBot\ThunBot\GuessWins.txt"
 
 #COOLDOWNS
 REPLY_COOLDOWN = 5      #IN SECONDS
